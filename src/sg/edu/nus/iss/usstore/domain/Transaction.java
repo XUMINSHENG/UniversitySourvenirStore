3adb2ba1783c2dd716c3776d7f49e5184cbb56d7
proliuxz <345832454@qq.com>
2015-03-16 16:21:09 +0800

    LXZ
    03-16 16:20

--- src/sg/edu/nus/iss/usstore/domain/Transaction.java
+++ src/sg/edu/nus/iss/usstore/domain/Transaction.java
@@ -1,16 +1,89 @@
 package sg.edu.nus.iss.usstore.domain;
 
-import java.awt.List;
+import java.util.Date;
+import java.util.List;
 
-import javax.xml.crypto.Data;
+
+
 
 public class Transaction {
 	private int id;
-	private Data date;
-	//private costomer contomer;
-	//private discount discount;
+	private Date date;
+	private String contomer;
+	private int discount;
 	private List itemList;
 	private double cashAmount;
 	private int redeemedLoyaltyPoint;
+	public Transaction()
+	{
+		
+	}
+	public Transaction(int id,List itemList,double cashAmount,int redeemedLoyaltyPoint)
+	{
+		this.id = id;
+		this.itemList = itemList;
+		this.cashAmount = cashAmount;
+		this.redeemedLoyaltyPoint = redeemedLoyaltyPoint;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public Date getDate()
+	{
+		return date;
+	}
+	public void setDate(Date date)
+	{
+		this.date = date;
+	}
+	public List getItemList()
+	{
+		return itemList;
+	}
+	public void setItemList(List itemList)
+	{
+		this.itemList = itemList;
+	}
+	public double getCashAmount()
+	{
+		return cashAmount;
+	}
+	public void setCashAmount(double cashAmount)
+	{
+		this.cashAmount = cashAmount;
+	}
+	public int getRedeemedLoyaltyPoint()
+	{
+		return redeemedLoyaltyPoint;
+	}
+	public void setRedeemedLoyaltyPoint(int redeemedLoyaltyPoint)
+	{
+		this.redeemedLoyaltyPoint = redeemedLoyaltyPoint;
+	}
+	public void addItem()
+	{}
+	public void removeItem()
+	{}
+	public double calcTotalPrice()
+	{
+		return 0;
+	}
+	public double calcDiscountPrice()
+	{
+		return calcTotalPrice()*discount;
+	}
+	public double calcChange()
+	{
+		return cashAmount-calcDiscountPrice();
+	}
+	public int calcGainedPoint()
+	{
+		return (int)(calcDiscountPrice()/100);
+	}
 	
 }
