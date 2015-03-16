4ef6ff23a505dc8fb2069d46554eb6e5fe7d4713
proliuxz <345832454@qq.com>
2015-03-16 13:31:27 +0800

    03161331
    Transaction

--- src/sg/edu/nus/iss/usstore/domain/Transaction.java
+++ src/sg/edu/nus/iss/usstore/domain/Transaction.java
@@ -1,5 +1,16 @@
 package sg.edu.nus.iss.usstore.domain;
 
-public class Transaction {
+import java.awt.List;
 
+import javax.xml.crypto.Data;
+
+public class Transaction {
+	private int id;
+	private Data date;
+	//private costomer contomer;
+	//private discount discount;
+	private List itemList;
+	private double cashAmount;
+	private int redeemedLoyaltyPoint;
+	
 }
