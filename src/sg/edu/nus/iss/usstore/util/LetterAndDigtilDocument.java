b4d1739c764b5af5c4c531062d973fa0310cf16d
xiejiabao <a0109328@u.nus.edu>
2015-03-20 18:55:33 +0800

    XIE JIABAO
    DigitDocument_update_3
    LetterAndDigtilocument_add

--- src/sg/edu/nus/iss/usstore/util/LetterAndDigtilDocument.java
+++ src/sg/edu/nus/iss/usstore/util/LetterAndDigtilDocument.java
@@ -4,32 +4,15 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 
-/*
- * digit input limited for JTextField
- * or with limited length of input
- * 
- * For example:
- * JTextField jtf = new JTextField();
- * jtf.setDocument(new DigitDocument())
- * or limits assigned length using int parameter,
- * jtf.setDocument(new DigitDocument(4));
- * 
- * Hint:
- * When setDocument for JTextfield, the data of JTextfield would be erased automatically.
- * So when u initial a JTextfield with a string, Using setText after setDocument to add your string.
- * 
- * @ XIE JIABAO
- */
+public class LetterAndDigtilDocument extends PlainDocument{
 
-public class DigitDocument extends PlainDocument{
+private int limitedLength;
 	
-	private int limitedLength;
-	
-	public DigitDocument(){
+	public LetterAndDigtilDocument(){
 		super();
 	}
 	
-	public DigitDocument(int length){
+	public LetterAndDigtilDocument(int length){
 		super();
 		this.limitedLength = length;
 	}
@@ -39,13 +22,12 @@ public class DigitDocument extends PlainDocument{
 		if(str == null){
 			return;
 		}
-		Character dot = new Character('.');
 		if(limitedLength<=0){
 			char[] upper = str.toCharArray();  
             int length = 0;  
             for (int i = 0; i < upper.length; i++)
             {  
-            	if (Character.isDigit(upper[i])||dot.equals(upper[i]))
+            	if (Character.isLetterOrDigit(upper[i]))
                 {  
                     upper[length++] = upper[i];
                 }  
@@ -57,7 +39,7 @@ public class DigitDocument extends PlainDocument{
 				int length = 0;  
 				for (int i = 0; i < upper.length; i++)
 				{  
-					if (Character.isDigit(upper[i])||dot.equals(upper[i]))
+					if (Character.isLetterOrDigit(upper[i]))
 					{  
 						upper[length++] = upper[i];  
 					}  
