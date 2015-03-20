4e9d0e2e814f2d156e2895b52cfaf12a1a3a7cfc
proliuxz <345832454@qq.com>
2015-03-20 21:32:34 +0800

    0320
    Test

---
+++ test/sg/edu/nus/iss/usstore/test/TransactionItemTest.java
@@ -0,0 +1,64 @@
+package sg.edu.nus.iss.usstore.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransactionItemTest
+{
+
+	@Test
+	public void testTransactionItemStringInt()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransactionItemProductDoubleInt()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProduct()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProduct()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrice()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQty()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetQty()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateAmount()
+	{
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/sg/edu/nus/iss/usstore/test/TransactionTest.java
@@ -0,0 +1,136 @@
+package sg.edu.nus.iss.usstore.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransactionTest
+{
+
+	@Test
+	public void testTransaction()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransactionIntStringDate()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDate()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDate()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItemList()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetItemList()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCashAmount()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCashAmount()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRedeemedLoyaltyPoint()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRedeemedLoyaltyPoint()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddItemProductInt()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddItemProductDoubleInt()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveItem()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcTotalPrice()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcDiscountPrice()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcChange()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcGainedPoint()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCostomerID()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCostomerID()
+	{
+		fail("Not yet implemented");
+	}
+
+}
