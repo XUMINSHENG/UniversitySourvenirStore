dcca03771092c63d035e571b0b77355c546c0e2d
proliuxz <345832454@qq.com>
2015-04-03 14:48:48 +0800

    LIU XINZHUO
    14:48

---
+++ test/sg/edu/nus/iss/usstore/test/TransactionMgrTest.java
@@ -0,0 +1,54 @@
+package sg.edu.nus.iss.usstore.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransactionMgrTest
+{
+
+	@Test
+	public void testGetTransactionList()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTransactionList()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSave()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTransactionMgrStore()
+	{
+		fail("Not yet implemented");
+	}
+
+	
+
+	@Test
+	public void testAddTransaction()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTransactionListByDate()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaxId()
+	{
+		fail("Not yet implemented");
+	}
+
+}
