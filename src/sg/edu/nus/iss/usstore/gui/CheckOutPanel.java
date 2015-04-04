62967ba79e4f01a9084dc658479339c0f3bcea39
tanujkhator <tanujkhator@gmail.com>
2015-04-04 14:12:19 +0800

    2.12

--- src/sg/edu/nus/iss/usstore/gui/CheckOutPanel.java
+++ src/sg/edu/nus/iss/usstore/gui/CheckOutPanel.java
@@ -753,29 +753,31 @@ public class CheckOutPanel extends JPanel
 			 * 	writes on the cansole the transaction in the below given format
 			 * after finishing of the transaction
 			 */
-				System.out.println("##############################################################################");
+				System.out.println("##################################################################################");
 				System.out.print(String.format("%1$-" + 30 + "s", "Tran ID : "+transaction.getId()));
 				System.out.println(String.format("%0$"+10+"s", "Date : "+transaction.getDate()));
 				System.out.print(String.format("%1$-" + 30 + "s", "Member ID : "+transaction.getCustomer().getID()));
 				System.out.println(String.format("%0$"+10+"s", "Loyalty Points : "+transaction.getRedeemedLoyaltyPoint()));
-				System.out.println("==========================================================================");
+				System.out.println("==================================================================================");
 				System.out.print(String.format("%1$-" + 50 + "s", "Product"));
 				System.out.print(String.format("%0$"+10+"s", String.format("%1$-" + 5 + "s", "Quantity")));
-				System.out.println(String.format("%0$"+10+"s", String.format("%1$-" + 5 + "s", "Price")));
-				System.out.println("===========================================================================");
+				System.out.print(String.format("%0$"+10+"s", String.format("%1$-" + 5 + "s", "Price")));
+				System.out.println(String.format("%0$"+14+"s", String.format("%1$-" + 5 + "s", "Total Price")));
+				System.out.println("===================================================================================");
 				for(TransactionItem item:transaction.getItemList()){
 				System.out.print(String.format("%1$-" + 50 + "s", item.getProduct().getBriefDescription()).substring(0, 50));
 				System.out.print(String.format("%0$"+7+"s", String.format("%1$-" + 5 + "s", item.getQty())));
-				System.out.println(String.format("%0$"+12+"s", String.format("%1$-" + 5 + "s", item.getPrice())));
-				System.out.println("-------------------------------------------------------------------------");
+				System.out.print(String.format("%0$"+12+"s", String.format("%1$-" + 5 + "s", item.getPrice())));
+				System.out.println(String.format("%0$"+12+"s", String.format("%1$-" + 5 + "s", item.calculateAmount())));
+				System.out.println("-------------------------------------------------------------------------------------");
 				}
-				System.out.print(String.format("%0$"+70+"s", "Total Price:"));
+				System.out.print(String.format("%0$"+70+"s", "Total Price   :"));
 				System.out.println(String.format("%0$"+5+"s", String.format("%1$-" + 5 + "s", Math.round(transaction.calcTotalPrice()))));
-				System.out.print(String.format("%0$"+70+"s", "Discount:"));
+				System.out.print(String.format("%0$"+70+"s", "Discount   :"));
 				System.out.println(String.format("%0$"+5+"s", String.format("%1$-" + 5 + "s", Math.round((transaction.calcTotalPrice()-transaction.calcDiscountPrice())))));
-				System.out.print(String.format("%0$"+70+"s", "Final Price:"));
+				System.out.print(String.format("%0$"+70+"s", "Final Price   :"));
 				System.out.println(String.format("%0$"+5+"s", String.format("%1$-" + 5 + "s",  Math.round( transaction.calcDiscountPrice()))));
-				System.out.println("##############################################################################");
+				System.out.println("#####################################################################################");
 				 	
 				
 				cancelAll();
