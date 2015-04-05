cf4f0f8147c25c9f37b549c35369e7b8371289c1
charan-a0134630r <a0134630@u.nus.edu>
2015-04-05 18:37:41 +0800

    Update compile.bat
    - Updated the .bat file to consider the jdatepicker jar file
    - Updated the .bat file to create bin directory if it does not exist.

--- compile.bat
+++ compile.bat
@@ -2,4 +2,5 @@ CD %~dp0
 CALL setenv.bat
 SET CUR_DIR=%CD%
 CD ./src/sg/edu/nus/iss/usstore
-javac -d %CUR_DIR%/bin util/*.java exception/*.java dao/*.java domain/*.java gui/*.java
+if not exist %CUR_DIR%/bin mkdir %CUR_DIR%/bin
+javac -d %CUR_DIR%/bin -cp %CUR_DIR%/lib/jdatepicker-1.3.4.jar util/*.java exception/*.java dao/*.java domain/*.java gui/*.java
