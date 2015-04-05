492348d27f008535cdb401a0207c2fdbc3532c5d
charan-a0134630r <a0134630@u.nus.edu>
2015-04-05 18:54:59 +0800

    Update compile.bat to use classes folder
    - Updated compile.bat to use classes folder instead of bin folder for
    storing the compiled classes.

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 CD %~dp0
 CALL setenv.bat
 SET CUR_DIR=%CD%
+if not exist classes mkdir classes
 CD ./src/sg/edu/nus/iss/usstore
-if not exist %CUR_DIR%/bin mkdir %CUR_DIR%/bin
-javac -d %CUR_DIR%/bin -cp %CUR_DIR%/lib/jdatepicker-1.3.4.jar util/*.java exception/*.java dao/*.java domain/*.java gui/*.java
+javac -d %CUR_DIR%/classes -cp %CUR_DIR%/lib/jdatepicker-1.3.4.jar util/*.java exception/*.java dao/*.java domain/*.java gui/*.java
