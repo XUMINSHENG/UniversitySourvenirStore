f89d51bbe3e47c2e49e64f159146e9fc9f10134f
charan-a0134630r <a0134630@u.nus.edu>
2015-04-05 22:12:38 +0800

    Update run.bat to consider external .jar
    - Updated the run.bat to consider the external .jar

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 CD %~dp0
 CALL setenv.bat
-java -classpath ./classes sg.edu.nus.iss.usstore.gui.StoreApplication
+java -classpath ./classes;./lib/jdatepicker-1.3.4.jar sg.edu.nus.iss.usstore.gui.StoreApplication
