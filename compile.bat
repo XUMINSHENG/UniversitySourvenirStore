0c0dbdb1d3e884734eefd0734ee5d86fe8549fbe
charan-a0134630r <a0134630@u.nus.edu>
2015-04-06 17:44:29 +0800

    Update compile.bat
    - Added device folder to the list of compile paths

--- compile.bat
+++ compile.bat
@@ -3,4 +3,4 @@ CALL setenv.bat
 SET CUR_DIR=%CD%
 if not exist classes mkdir classes
 CD ./src/sg/edu/nus/iss/usstore
-javac -d %CUR_DIR%/classes -cp %CUR_DIR%/lib/jdatepicker-1.3.4.jar util/*.java exception/*.java dao/*.java domain/*.java gui/*.java
+javac -d %CUR_DIR%/classes -cp %CUR_DIR%/lib/jdatepicker-1.3.4.jar dao/*.java device/*.java domain/*.java exception/*.java gui/*.java util/*.java
